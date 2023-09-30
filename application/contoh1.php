7ac941adcb51cf8c655e695c707a298b01ed4d5b
devitanggun <devitanggun23@gmail.com>
2023-10-01 00:15:28 +0700

    first commit

--- application/contoh1.php
+++ application/contoh1.php
@@ -4,10 +4,9 @@ class contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>perkenalkan</h1>";
-        echo "Nama : Devita Anggun Ashari
+        echo"Nama : Devita Anggun Ashari
              Nim : 19220110
              Kelas 19.3A.26
              Alamat : Slipi";
     }
 }
-</php>
