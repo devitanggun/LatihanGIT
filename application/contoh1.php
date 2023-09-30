3ab69f578a8e71445938ffa3ac13248d89396d46
devitanggun <devitanggun23@gmail.com>
2023-09-30 22:21:34 +0700

    menambahkan file

---
+++ application/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+Class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo"Nama : Devita Anggun Ashari
+             Nim : 19220110
+             Kelas 19.3A.26
+             Alamat : Slipi";
+    }
+}
---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh 1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo"Nama: Devita Anggun Ashari
+             Nim: 19220110
+             Alamat: JL. KS TUBUN III";
+    }
+}
