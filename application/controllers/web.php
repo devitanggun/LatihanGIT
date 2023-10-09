573b65a7d0e8369fbd6f1531f0254058de0c9724
devitanggun <devitanggun23@gmail.com>
2023-10-09 15:12:29 +0700

    menambahkan folder bru

--- application/controllers/web.php
+++ application/controllers/web.php
@@ -2,6 +2,7 @@
 defined('BASEPATH') or exit ('no direct script access allowed');
 
 class Web extends CI_Controller{
+
     function __construct(){
         parent::__construct();
     }
@@ -9,6 +10,12 @@ class Web extends CI_Controller{
         $data['judul'] = "Halaman Depan";
         $this->load->view('v_header',$data);
         $this->load->view('v_index',$data);
+        $this->load->view('v_footer',$data);
+    }
+    public function about()(
+        $data['judul'] = "about";
+        $this->load->view('v_header',$data);
+        $this->load->view('v_about',$data);
         $this->load->view('v_footer',$data);
     }
 }
